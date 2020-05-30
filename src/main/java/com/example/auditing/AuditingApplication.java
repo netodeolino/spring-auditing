45608e8281fb3a151cf1280267c10c0bb6a50d6d
Neto Deolino <netodeolino@gmail.com>
2020-05-30 10:25:13 -0300

    started project

---
+++ src/main/java/com/example/auditing/AuditingApplication.java
@@ -0,0 +1,13 @@
+package com.example.auditing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditingApplication.class, args);
+	}
+
+}
